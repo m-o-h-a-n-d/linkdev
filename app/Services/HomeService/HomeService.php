5ec78dca36a1a4cc68e1abd6e7d566d3107e66ce
Youssef Elbrmbaly <149072029+Yossef-Elbrmbaly@users.noreply.github.com>
2026-08-12 01:35:03 +0300

    feat(home): add live matches retrieval

--- app/Services/HomeService/HomeService.php
+++ app/Services/HomeService/HomeService.php
@@ -4,6 +4,7 @@ namespace App\Services\HomeService;
 
 use App\Repositories\Contracts\Competition\CompetitionRepositoryInterface;
 use App\Repositories\Contracts\Match\MatchRepositoryInterface;
+use App\Repositories\Contracts\Team\TeamRepositoryInterface;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
@@ -12,7 +13,8 @@ class HomeService
 {
     public function __construct(
         protected MatchRepositoryInterface $matchRepository,
-        protected CompetitionRepositoryInterface $competitionRepository
+        protected CompetitionRepositoryInterface $competitionRepository,
+        protected TeamRepositoryInterface $teamRepository
     ) {}
 
     public function getCompetitions(): Collection
@@ -20,8 +22,23 @@ class HomeService
         return $this->competitionRepository->all();
     }
 
+    public function getMatches(): Collection
+    {
+        return $this->matchRepository->all();
+    }
+
+    public function getTeams(): Collection
+    {
+        return $this->teamRepository->all();
+    }
+
     public function getUpcomingMatches(int $limit = 3): Collection
     {
         return $this->matchRepository->getUpcomingMatches($limit);
+    }
+
+    public function getLiveMatches(): Collection
+    {
+        return $this->matchRepository->getLiveMatches();
     }
 }
